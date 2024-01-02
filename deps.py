3fc9f897df232761e2e2136cf4ec5de01de17854
javad <j.2528840@gmail.com>
2024-01-02 11:43:36 +0330

    edit

--- deps.py
+++ deps.py
@@ -1,36 +1,17 @@
+import json
 from web3 import Web3, EthereumTesterProvider, HTTPProvider, WebsocketProvider, IPCProvider
 from eth_account.messages import encode_defunct, _hash_eip191_message
 from web3.middleware import geth_poa_middleware
 from web3.middleware import construct_sign_and_send_raw_middleware
 
-# w3 = Web3(HTTPProvider('https://bsc-dataseed.binance.org/'))
-
-# w3 = Web3(HTTPProvider('https://data-seed-prebsc-1-s1.bnbchain.org:8545'))
-
-
-# w3 = Web3(HTTPProvider('https://rpc.ankr.com/eth_sepolia/881e0ccae3d24ffa4eca05ac7022fb861928b642290d9cf480fcda6ead80f5f5'))
-
 
 w3 = Web3(HTTPProvider('https://api.avax-test.network/ext/bc/C/rpc'))
-
-
-# w3 = Web3(EthereumTesterProvider())
-
-
-# w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 if w3.is_connected():
     print("Connected to Ethereum node")
 else:
     print("Failed to connect to Ethereum node")
 
-
-# create test account
-# acc = w3.eth.account.create()
-# address = acc.address
-# private_key = acc.key.hex()
-# print(address)
-# print(private_key)
 
 address = '0x99baFA590a24755f94F5815df06102C2450AcD3D'
 private_key = 'b0f075c393877e332ed691e1341c3897c5b7a28e20861eed200bcdb199c0e745'
@@ -64,13 +45,11 @@ private_key2 = '0xde4f925a22929a8766e16d921b4123c78561b61e6a5cb1bdbb60cf5705de7c
 
 # amount = w3.to_wei(0.0001, 'ether')
 
-# # تعیین اطلاعات تراکنش
 # nonce = w3.eth.get_transaction_count(address)
 # gas_price = w3.eth.gas_price
 # gas_limit = 21000
 # value = amount
 
-# # ساخت تراکنش
 # transaction = {
 #     'chainId': 11155111,
 #     'from': address,
@@ -81,80 +60,61 @@ private_key2 = '0xde4f925a22929a8766e16d921b4123c78561b61e6a5cb1bdbb60cf5705de7c
 #     'nonce': nonce,
 # }
 
-# # امضای تراکنش
 # signed_tx = w3.eth.account.sign_transaction(transaction, private_key=private_key)
 
-# # ارسال تراکنش
 # tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
 # print(f'Transaction sent: {tx_hash.hex()}')
 
 
-# کلید خصوصی حساب مبدأ
 private_key_from = private_key
 
-# آدرس حساب مبدأ
 address_from = address
 
-# کلید خصوصی حساب مقصد
 private_key_to = private_key2
 nonce = w3.eth.get_transaction_count(address_from)
 max_priority_fee_per_gas = w3.to_wei('5', 'gwei')
-# آدرس حساب مقصد
 address_to = address1
 
-# مبلغ فانتوم برای انتقال
 amount = w3.to_wei(0.0001, 'ether')
 
-# ایجاد یک تراکنش نوع 2
 tx = {'to': '0x2392bAE123ECd96eE997Be6BFA43d9AE98039BDC', 'value': 100000000000000, 'gas': 21000,
       'maxPriorityFeePerGas': 5000000000, 'maxFeePerGas': 25000000000, 'chainId': 4002, 'nonce': 6, 'type': 2}
 
 
-# امضای تراکنش با کلید خصوصی حساب مبدأ
 signed_tx = w3.eth.account.sign_transaction(tx, private_key_from)
-print(signed_tx)
-# # ارسال تراکنش امضا شده به شبکه فانتوم
-# tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-# print(f'Transaction hash: {w3.to_hex(tx_hash)}')
 
-# import json
+tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+print(f'Transaction hash: {w3.to_hex(tx_hash)}')
 
 
-# non_checksum_address = '0x5425890298aed601595a70AB815c96711a31Bc65'
-# token_address = w3.to_checksum_address(non_checksum_address)
-# # ---------------------------------------------------------------------------------
+non_checksum_address = '0x5425890298aed601595a70AB815c96711a31Bc65'
+token_address = w3.to_checksum_address(non_checksum_address)
+# ---------------------------------------------------------------------------------
 
-# # اطلاعات کیف پول فرستنده
-# sender_address = address
-# sender_private_key = private_key
+sender_address = address
+sender_private_key = private_key
 
-# with open('abi.json') as f:
-#     token_abi = json.load(f)['abi']
+with open('abi.json') as f:
+    token_abi = json.load(f)['abi']
 
-# # اطلاعات گیرنده توکن
-# recipient_address = address1
+recipient_address = address1
 
-# # انتقال توکن
-# token_contract = w3.eth.contract(address=token_address, abi=token_abi)
-# nonce = w3.eth.get_transaction_count(sender_address)
+token_contract = w3.eth.contract(address=token_address, abi=token_abi)
+nonce = w3.eth.get_transaction_count(sender_address)
 
-# transfer = token_contract.functions.transfer(recipient_address, 1000)
-# tx = transfer.build_transaction({
-#     'from': sender_address,
-#     'nonce': nonce,
-#     'gas': 100000,
-#     'gasPrice': w3.to_wei('50', 'gwei')
-# })
+transfer = token_contract.functions.transfer(recipient_address, 1000)
+tx = transfer.build_transaction({
+    'from': sender_address,
+    'nonce': nonce,
+    'gas': 100000,
+    'gasPrice': w3.to_wei('50', 'gwei')
+})
 
-# signed_tx = w3.eth.account.sign_transaction(tx, sender_private_key)
-# tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-# print(f'Transaction hash: {w3.to_hex(tx_hash)}')
+signed_tx = w3.eth.account.sign_transaction(tx, sender_private_key)
+tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+print(f'Transaction hash: {w3.to_hex(tx_hash)}')
 
 
-# balance = token_contract.functions.balanceOf(sender_address).call()
-# print(f"Your balance is: {balance}")
-
-# # ------------------------------------------------------------------------------------
-# balance = w3.eth.get_balance(address)
-# print(balance)
+balance = token_contract.functions.balanceOf(sender_address).call()
+print(f"Your balance is: {balance}")
